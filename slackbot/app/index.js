8607414a2c4e506a5aaa6ae7038a5996d84c6af4
Pavel Milkevich <pavel.milkevich@gmail.com>
2017-04-21 10:46:49 +0300

    added external settings

--- slackbot/app/index.js
+++ slackbot/app/index.js
@@ -1,22 +1,23 @@
 // Require libraries.
 const botkit = require('botkit');
+const settings = require('../../settings');
 
 // Create slackbot controller.
 const controller = botkit.slackbot({
-  debug: false,
-  send_via_rtm: true,
-  json_file_store: './db'
+    debug:           true,
+    send_via_rtm:    true,
+    json_file_store: './db'
 });
 
 // Create the slackbot.
 const slackBot = controller.spawn({
-  // IMPORTANT! Do not check in this token to GIT.
-  token: 'TOKEN_HERE'
+    // IMPORTANT! Do not check in this token to GIT.
+    token: settings.token
 });
 
 // Listen for a direct message.
-controller.hears([/^hello robot$/i], ['direct_message'], function(bot, message) {
-  bot.reply(message, 'Hello Human!');
+controller.hears([/^hello robot$/i], ['direct_message'], function (bot, message) {
+    bot.reply(message, 'Hello Human!');
 });
 
 // Start real-time messaging.
